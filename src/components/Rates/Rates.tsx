e1622ad92a7dcdcab70710d205ed7ab305a3bb4c
Thintalltom <adeyanjutomide@gmail.com>
2025-05-17 17:00:21 +0100

    added scrolling animation

--- src/components/Rates/Rates.tsx
+++ src/components/Rates/Rates.tsx
@@ -73,30 +73,49 @@ const Rates = () => {
     },
   ];
   return (
-    <div className="min-w-full xs:p-[20px]  bg-white rounded-[10px] border-[1px] ">
+    <div className="min-w-full xs:p-[20px]  bg-white rounded-[10px] border-[1px] overflow-hidden">
       <div className="flex justify-between">
         <p className="font-bold ">FX Rates</p>
         <p className="text-gray-400 text-sm">See all</p>
       </div>
-      <div className="flex flex-row gap-[20px]  overflow-x-auto">
-        {data.map((data, index) => (
-          <div key={index} className="flex flex-row gap-[10px]">
-            <div className="bg-gray-100 px-[5px] py-[5px] w-[100px] rounded-[10px]  h-fit flex flex-col gap-[5px]">
-              <div className="flex text-[10px] flex-row gap-[10px] justify-between">
-                <p>{data.name}</p>
-                <p>{data.short}</p>
-              </div>
-              <div className="flex justify-center text-sm items-center">
-                {data.logo}
+      <div className="w-full relative">
+        <div className="flex animate-marquee whitespace-nowrap">
+          {data.map((item, index) => (
+            <div key={index} className="flex-shrink-0 mr-[20px]">
+              <div className="bg-gray-100 px-[5px] py-[5px] w-[100px] rounded-[10px] h-fit flex flex-col gap-[5px]">
+                <div className="flex text-[10px] flex-row gap-[10px] justify-between">
+                  <p>{item.name}</p>
+                  <p>{item.short}</p>
+                </div>
+                <div className="flex justify-center text-sm items-center">
+                  {item.logo}
                 </div>
                 <div className="flex flex-row gap-[10px] text-[10px] justify-between">
-                     <p>₦{data.price}</p>
-                <p>${data.start_price}</p>
-               
+                  <p>₦{item.price}</p>
+                  <p>${item.start_price}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+             {data.map((item, index) => (
+            <div key={index} className="flex-shrink-0 mr-[20px]">
+              <div className="bg-gray-100 px-[5px] py-[5px] w-[100px] rounded-[10px] h-fit flex flex-col gap-[5px]">
+                <div className="flex text-[10px] flex-row gap-[10px] justify-between">
+                  <p>{item.name}</p>
+                  <p>{item.short}</p>
+                </div>
+                <div className="flex justify-center text-sm items-center">
+                  {item.logo}
+                </div>
+                <div className="flex flex-row gap-[10px] text-[10px] justify-between">
+                  <p>₦{item.price}</p>
+                  <p>${item.start_price}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        
       </div>
     </div>
   );
